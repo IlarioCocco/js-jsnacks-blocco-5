cc2890bf99fed487dd3b2bb2937dee8adca270a4
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-08 11:18:55 +0200

    commit

---
+++ snack 1/js/scripts.js
@@ -0,0 +1,9 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando
+// per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+
+
+let zucchineContenitore = [
+    { pesoGr: 50, lunghezzaCm: 20},
+   
+]
