f18a7b786bd679b6e2b5bc9b07982da0a0739d25
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-08 20:56:03 +0200

    commit

--- snack 5/js/scripts.js
+++ snack 5/js/scripts.js
@@ -6,29 +6,30 @@
 
 // esercizio snack 5
 
-
 // se l'array iniizale è ad esempio:
 // var array = [0, 1, 2, 3, 4, 5, 6];
 // Creo una funzione con un minimo e massimo ad esempio min: 1 max 3 che mi darà come risultato:
 // var array2 = [1, 2, 3];
 
+// Scrivi una funzione che accetti tre argomenti
+// un array e due numeri(“a” più piccolo di “b” e “b” grande al massimo
+// quanto il numero di elementi dell’array).
 let array_primario = [1,2,3,4,5,6,7,8,9,10];
 let a = 2;
 let b = 7;
 
-
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione
+// compresa tra “a” e “b”
 function soluzione(array_iniziale,num1,num2) {
     let array_secondario = [];
     for (let i = 0; i < array_iniziale.length; i++) {
-
-
-        
-        
+    if (array_iniziale[i] < num1, num2);
+        array_secondario.push(num1, num2);
+    console.log(array_secondario)
     }
-
+    return array_secondario;
 }   
 
 
-
-var arrayNuovo = soluzione(array_primario, a, b);
-console.log(arrayNuovo)
+    var arrayNuovo = soluzione(array_primario, a, b);
+console.log(arrayNuovo);
