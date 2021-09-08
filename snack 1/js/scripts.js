27f40bcc18bf05130627828cad0d14be07c2a173
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-08 11:39:53 +0200

    commit

--- snack 1/js/scripts.js
+++ snack 1/js/scripts.js
@@ -4,14 +4,20 @@
 
 
 let zucchineContenitore = [
-    { pesoGr: 50, lunghezzaCm: 20},
-    { pesoGr: 50, lunghezzaCm: 20},
-    { pesoGr: 50, lunghezzaCm: 20},
-    { pesoGr: 50, lunghezzaCm: 20},
-    { pesoGr: 50, lunghezzaCm: 20},
-    { pesoGr: 50, lunghezzaCm: 20},
-    { pesoGr: 50, lunghezzaCm: 20 },
-    { pesoGr: 50, lunghezzaCm: 20 },
-    { pesoGr: 50, lunghezzaCm: 20 },
-    { pesoGr: 50, lunghezzaCm: 20 },
+    { tipo: 1, pesoGr: 50, lunghezzaCm: 20},
+    { tipo: 2, pesoGr: 50, lunghezzaCm: 21},
+    { tipo: 3, pesoGr: 50, lunghezzaCm: 22},
+    { tipo: 4, pesoGr: 50, lunghezzaCm: 23},
+    { tipo: 5, pesoGr: 50, lunghezzaCm: 24},
+    { tipo: 6, pesoGr: 50, lunghezzaCm: 25},
+    { tipo: 7, pesoGr: 50, lunghezzaCm: 26},
+    { tipo: 8, pesoGr: 50, lunghezzaCm: 27},
+    { tipo: 9, pesoGr: 50, lunghezzaCm: 28},
+    { tipo: 10, pesoGr: 50, lunghezzaCm: 29},
 ]
+
+// let zucchineSomma= {};
+let pesoGr = 50
+let lunghezzaCm = 20
+let zucchineSomma = pesoGr + 100;
+console.log(zucchineSomma);
