ed9920890a925b8ccbae4a292edd4beba586a764
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-08 17:52:46 +0200

    commit snack 5 recap

--- snack 2/js/scripts.js
+++ snack 2/js/scripts.js
@@ -10,8 +10,7 @@
 // Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 
 
-let zucchine_contenitore = [
-
+const zucchine_contenitore = [
     { tipo: 1, pesoGr: 50, lunghezzaCm: 10 },
     { tipo: 2, pesoGr: 50, lunghezzaCm: 12 },
     { tipo: 3, pesoGr: 50, lunghezzaCm: 14 },
@@ -20,8 +19,9 @@ let zucchine_contenitore = [
     { tipo: 6, pesoGr: 50, lunghezzaCm: 17 },
     { tipo: 7, pesoGr: 50, lunghezzaCm: 18 },
     { tipo: 8, pesoGr: 50, lunghezzaCm: 19 },
-]
+];
 
+//ho creato 2 contenitori
 let zuc_corte = []; 
 let zuc_lunghe = [];
 
@@ -30,8 +30,8 @@ let zuc_lunghe_peso = 0;
 
 
 for (let i = 0; i < zucchine_contenitore.length; i++) {
+
         //If it is less than 15 cm Push into the short zucchine array
-    
     if (zucchine_contenitore[i].lunghezzaCm < 15){
         zuc_corte.push(zucchine_contenitore[i]);
         zuc_corte_peso += zucchine_contenitore[i].pesoGr;
@@ -43,28 +43,13 @@ for (let i = 0; i < zucchine_contenitore.length; i++) {
     }
 }
 
+//stampo in console.log separatamente le zucchine che misurano meno di 15 cm
 console.log("zucchine corte:" + " " + zuc_corte);
 console.log("zucchine lunghe:" + " " + zuc_lunghe);
-
 
 console.log("zucchine corte peso:" + " " + zuc_corte_peso);
 console.log("zucchine lunghe peso:" + " " + zuc_lunghe_peso);
 
-
-// for (let i = 0; i < zucchineContenitoreDue.length; i++) {
-//     totalePesoDue += zucchineContenitoreDue[i].peso;
-// }
-// console.log(totalePesoDue);
-
-
-// let zucchineContenitoreDue = [
-//     { tipo: 5, peso: 50, lunghezzaCm: 16 },
-//     { tipo: 6, peso: 50, lunghezzaCm: 17 },
-//     { tipo: 7, peso: 50, lunghezzaCm: 18 },
-//     { tipo: 8, peso: 50, lunghezzaCm: 19 },
-//     { tipo: 9, peso: 50, lunghezzaCm: 20 },
-//     { tipo: 10, peso: 50, lunghezzaCm: 21 },
-// ]
 
 
 
--- snack 5/js/scripts.js
+++ snack 5/js/scripts.js
@@ -5,3 +5,30 @@
 // compresa tra “a” e “b”
 
 // esercizio snack 5
+
+
+// se l'array iniizale è ad esempio:
+// var array = [0, 1, 2, 3, 4, 5, 6];
+// Creo una funzione con un minimo e massimo ad esempio min: 1 max 3 che mi darà come risultato:
+// var array2 = [1, 2, 3];
+
+let array_primario = [1,2,3,4,5,6,7,8,9,10];
+let a = 2;
+let b = 7;
+
+
+function soluzione(array_iniziale,num1,num2) {
+    let array_secondario = [];
+    for (let i = 0; i < array_iniziale.length; i++) {
+
+
+        
+        
+    }
+
+}   
+
+
+
+var arrayNuovo = soluzione(array_primario, a, b);
+console.log(arrayNuovo)
