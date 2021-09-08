bdb4b62a6250cd5ed9f0e435f056a0f56e4ab7ce
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-08 15:32:18 +0200

    commit correzione peso esercizio  snack 2

--- snack 2/js/scripts.js
+++ snack 2/js/scripts.js
@@ -15,30 +15,41 @@ let zucchineContenitore = [
     { tipo: 2, pesoGr: 50, lunghezzaCm: 12 },
     { tipo: 3, pesoGr: 50, lunghezzaCm: 14 },
     { tipo: 4, pesoGr: 50, lunghezzaCm: 15 },
-]
 
-let zucchineContenitoreDue = [
     { tipo: 5, peso: 50, lunghezzaCm: 16 },
     { tipo: 6, peso: 50, lunghezzaCm: 17 },
     { tipo: 7, peso: 50, lunghezzaCm: 18 },
     { tipo: 8, peso: 50, lunghezzaCm: 19 },
-    { tipo: 9, peso: 50, lunghezzaCm: 20 },
-    { tipo: 10, peso: 50, lunghezzaCm: 21 },
-] 
+]
 
+let zuc_corte = []; 
+let zuc_lunghe = [];
+let zuc_corte_peso = 0;
+let zuc_lunghe_peso = 0;
 
 for (let i = 0; i < zucchineContenitore.length; i++) {
+    //If it is less than 15 cm Push into the short zucchini array
     if (zucchineContenitore[i].lunghezzaCm < 15){
-        zucchineContenitoreDue.push(zucchineContenitore[i]);
+        zuc_corte.push(zucchineContenitore[i]);
+        zuc_corte_peso += zucchineContenitore[i].pesoGr;
     } else {
-        zucchineContenitore.push(zucchineContenitore[i]);
+        zuc_lunghe.push(zucchineContenitore[i]);
+        zuc_lunghe_peso += zucchineContenitore[i].pesoGr;
     }
 }
 
-console.log(zucchineContenitore);
-console.log(zucchineContenitoreDue);
+console.log("zucchine corte:" + " " + zuc_corte);
+console.log("zucchine lunghe:" + " " + zuc_lunghe);
 
 
+console.log("zucchine corte peso:" + " " + zuc_corte_peso);
+console.log("zucchine lunghe peso:" + " " + zuc_lunghe_peso);
+
+
+// for (let i = 0; i < zucchineContenitoreDue.length; i++) {
+//     totalePesoDue += zucchineContenitoreDue[i].peso;
+// }
+// console.log(totalePesoDue);
 
 
 
@@ -50,11 +61,8 @@ console.log(zucchineContenitoreDue);
 //     { tipo: 9, peso: 50, lunghezzaCm: 20 },
 //     { tipo: 10, peso: 50, lunghezzaCm: 21 },
 // ]
-// let totalePesoDue = 0;
 
-// for (let i = 0; i < zucchineContenitoreDue.length; i++) {
-//     totalePesoDue += zucchineContenitoreDue[i].peso;
-// }
-// console.log(totalePesoDue);
+
+
 
 
