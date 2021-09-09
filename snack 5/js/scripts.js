9744293d7d69cfad639478ca4db7e56598a41174
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-09 15:47:42 +0200

    commit

--- snack 5/js/scripts.js
+++ snack 5/js/scripts.js
@@ -14,24 +14,21 @@
 // Scrivi una funzione che accetti tre argomenti
 // un array e due numeri(“a” più piccolo di “b” e “b” grande al massimo
 // quanto il numero di elementi dell’array).
-let array_primario = [1,2,3,4,5,6,7,8,9,10];
-let a = 2;
-let b = 7;
+let array_primario = ["a", "b", "c", "d", "e"];
+
+let possibileSoluzione = soluzione(array_primario, 2, 4);
+console.log(possibileSoluzione);
 
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione
 // compresa tra “a” e “b”
-function soluzione(array_iniziale,num1,num2) {
+function soluzione(array_primario, num1, num2) {
     let array_secondario = [];
-    for (let i = 0; i < array_iniziale.length; i++) {
-
-    // if (array_iniziale[i] < num1, num2);
-    //     array_secondario.push(num1, num2);
-    // console.log(array_secondario)
-
+    for (let i = 0; i < array_primario.length; i++) {
+        //console.log()array_primario[i];
+        if (i >= num1  && i <= num2) {
+            array_secondario.push(array_primario[i]);
+        }
+    
     }
-
     return array_secondario;
 }   
-
-    var arrayNuovo = soluzione(array_primario, a, b);
-console.log(arrayNuovo);
